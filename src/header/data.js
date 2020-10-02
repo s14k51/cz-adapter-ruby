f57e4c4983ff03970359722d4981c2f1b98928aa
s14k51 <s14k51@gmail.com>
2020-10-02 13:04:13 -0500

    feat: update conventional types

--- src/header/data.js
+++ src/header/data.js
@@ -1,9 +1,9 @@
-const { conventionalRubyTypes, conventionalRubyTypesInfo } = require('conventional-ruby-commit-types');
+const { types, typesInfo } = require('conventional-ruby-commit-types');
 const longest = require('longest');
 
-const length = longest(conventionalRubyTypes).length + 1;
-const supportedTypes = conventionalRubyTypes.map((type) => ({
-  name: `${(`${type}:`).padEnd(length)} ${conventionalRubyTypesInfo[type].description}`,
+const length = longest(types).length + 1;
+const supportedTypes = types.map((type) => ({
+  name: `${(`${type}:`).padEnd(length)} ${typesInfo[type].description}`,
   value: type,
 }));
 
